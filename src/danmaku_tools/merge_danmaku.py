dca1897cba8d7b06518c0876bf6654a23a3c6f4a
Shawn Sun <lengyanyu258@outlook.com>
2023-06-13 17:00:07 +0800

    Replace ET by DOM
    ensure doesn't drop xml stylesheet declarations in output file
    so that we can read xml file human readable in brower

--- src/danmaku_tools/merge_danmaku.py
+++ src/danmaku_tools/merge_danmaku.py
@@ -1,6 +1,6 @@
 import argparse
 import subprocess
-import xml.etree.ElementTree as ET
+import xml.dom.minidom as DOM
 from dateutil.parser import parse
 
 parser = argparse.ArgumentParser(description='Merge BiliLiveReocrder XML')
@@ -10,27 +10,27 @@ parser.add_argument('--output', type=str, default=None, help='output path for th
 
 
 def get_root_time(root_xml):
-    record_info = root_xml.findall('BililiveRecorderRecordInfo')[0]
-    record_start_time_str = record_info.attrib['start_time']
+    record_info = root_xml.getElementsByTagName('BililiveRecorderRecordInfo')[0]
+    record_start_time_str = record_info.getAttribute('start_time')
     record_start_time = parse(record_start_time_str)
     return record_start_time
 
 
 def add_root(orig_root, new_root, new_offset):
-    for child in new_root:
-        if child.tag in ['sc', 'gift', 'guard']:
-            orig_time = float(child.attrib['ts'])
+    for child in new_root.childNodes:
+        if child.nodeName in ['sc', 'gift', 'guard']:
+            orig_time = float(child.getAttribute('ts'))
             new_time = orig_time + new_offset
             new_time_str = str(new_time)
-            child.set('ts', new_time_str)
-            orig_root.append(child)
-        if child.tag in ['d']:
-            orig_parameters_str = child.attrib['p'].split(',')
+            child.setAttribute('ts', new_time_str)
+            orig_root.appendChild(child)
+        elif child.nodeName in ['d']:
+            orig_parameters_str = child.getAttribute('p').split(',')
             orig_time = float(orig_parameters_str[0])
             new_time = orig_time + new_offset
             new_parameters_str = [str(new_time)] + orig_parameters_str[1:]
-            child.set('p', ','.join(new_parameters_str))
-            orig_root.append(child)
+            child.setAttribute('p', ','.join(new_parameters_str))
+            orig_root.appendChild(child)
 
 
 if __name__ == '__main__':
@@ -39,13 +39,13 @@ if __name__ == '__main__':
     if len(args.xml_files) == 0:
         print("At least one XML files have to be passed as input.")
 
-    tree = ET.parse(args.xml_files[0])
-    root = tree.getroot()
+    doc = DOM.parse(args.xml_files[0])
+    root = doc.documentElement
     new_root_offset = 0
-    all_flv = ""
+    # all_flv = ""
 
     for i in range(len(args.xml_files) - 1):
-        new_root = ET.parse(args.xml_files[i + 1]).getroot()
+        new_root = DOM.parse(args.xml_files[i + 1]).documentElement
         if args.video_time == "":
             root_time = get_root_time(root)
             new_root_offset = (get_root_time(new_root) - root_time).total_seconds()
@@ -57,8 +57,9 @@ if __name__ == '__main__':
                 f'ffprobe -v error -show_entries format=duration '
                 f'-of default=noprint_wrappers=1:nokey=1 "{flv_file_path}"', shell=True
             )
-            all_flv += flv_file_path + "\n"
+            # all_flv += flv_file_path + "\n"
             new_root_offset += float(total_seconds_str)
         add_root(root, new_root, new_root_offset)
 
-    tree.write(args.output, encoding='UTF-8', xml_declaration=True)
+    with open(args.output, 'w', encoding='utf-8') as f:
+        doc.writexml(f, indent='', addindent='', newl='', encoding="utf-8")
